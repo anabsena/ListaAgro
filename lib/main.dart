92c8802ddd4a351c0cc7b5e745404d959178f63d
anabsena <anabsenaf@gmail.com>
2023-11-26 23:18:58 -0300

    trabalho

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:trabalho_agro/screens/listagem_despesas.dart';
 import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options:DefaultFirebaseOptions.currentPlatform
   );
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
